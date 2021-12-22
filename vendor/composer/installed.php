cb0e0c2ede1661bd9637a1bcdd901f2472a2f272
Mike Hemberger <mike@thestizmedia.com>
2021-12-21 19:44:43 -0500

    make sure wpdi is loaded from engine

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,29 +6,17 @@
     'aliases' => 
     array (
     ),
-    'reference' => '12f6c9b34aa2064bd4fd4231df796ac0895e7790',
+    'reference' => '1aa3eedc2ae5768e88e2770ce156077fda2cb2e7',
     'name' => 'maithemewp/mai-reach',
   ),
   'versions' => 
   array (
     'afragen/wp-dependency-installer' => 
     array (
-      'pretty_version' => '4.2.2',
-      'version' => '4.2.2.0',
-      'aliases' => 
+      'replaced' => 
       array (
+        0 => '*',
       ),
-      'reference' => '5e5419348c6cde7b02f3c33fb3fc3381062443a1',
-    ),
-    'afragen/wp-dismiss-notice' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '21949732f1ea21d81500bdda8f882ac7eb0f98af',
     ),
     'maithemewp/mai-installer' => 
     array (
@@ -38,7 +26,7 @@
       array (
         0 => '9999999-dev',
       ),
-      'reference' => '244cf7604a6b34a4662c29385497d4257f175956',
+      'reference' => '1b637076d114af7762855493b9bac009a6272d5c',
     ),
     'maithemewp/mai-reach' => 
     array (
@@ -47,7 +35,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '12f6c9b34aa2064bd4fd4231df796ac0895e7790',
+      'reference' => '1aa3eedc2ae5768e88e2770ce156077fda2cb2e7',
     ),
   ),
 );
